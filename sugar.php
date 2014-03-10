5f9df899512b5ff05650b08dbc8e2d96dc2a0653
Kyle Tucker <kyleatucker@gmail.com>
2014-03-10 14:15:56 -0400

    Plugin doc setup

---
+++ sugar.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Sugar
+ * Description: Sweeten the WordPress core with functions you always wish you had.
+ * Author:      ASMBS
+ * Author URI:  https://github.com/asmbs
+ * License:     MIT License
+ * License URI: http://opensource.org/licenses/MIT
+ * Version:     0.1.0-beta
+ */
