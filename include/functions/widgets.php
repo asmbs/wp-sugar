750ce1f7d5e3dcf54ca6186c6ed325b15da57243
Kyle Tucker <kyleatucker@gmail.com>
2014-03-19 14:42:32 -0400

    Unregister the rest of the widgets

--- include/functions/widgets.php
+++ include/functions/widgets.php
@@ -10,7 +10,17 @@
  * ------------------------------------------------------------------------------------------ */
 function unregister_bloggy_widgets()
 {
-  unregister_widget( 'WP_Widget_Calendar' );
   unregister_widget( 'WP_Widget_Archives' );
+  unregister_widget( 'WP_Widget_Calendar' );
+  unregister_widget( 'WP_Widget_Categories' );
+  unregister_widget( 'WP_Widget_Meta' );
+  unregister_widget( 'WP_Widget_Pages' );
+  unregister_widget( 'WP_Widget_Recent_Comments' );
+  unregister_widget( 'WP_Widget_Recent_Posts' );
+  unregister_widget( 'WP_Widget_RSS' );
+  unregister_widget( 'WP_Widget_Search' );
+  unregister_widget( 'WP_Widget_Tag_Cloud' );
+
+  unregister_widget( 'WP_Nav_Menu_Widget' );
 }
 add_action( 'widgets_init', 'unregister_bloggy_widgets' );
