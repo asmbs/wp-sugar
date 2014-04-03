9f62044924f51a7059b58a75db0acf526e33c3ea
Kyle Tucker <kyleatucker@gmail.com>
2014-04-03 10:44:40 -0400

    Add link-by-slug shortcode

--- sugar.php
+++ sugar.php
@@ -13,6 +13,7 @@
 define( 'SUGAR_PATH', plugin_dir_path( __FILE__ ) );
 
 // Include stuff
+include_once SUGAR_PATH .'include/functions/shortcode.php';
 include_once SUGAR_PATH .'include/functions/template.php';
 include_once SUGAR_PATH .'include/functions/utilities.php';
 include_once SUGAR_PATH .'include/functions/widgets.php';
