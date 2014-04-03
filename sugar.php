2eff34a5e5775b7ddf10b716e03b6a1671fb3a90
Kyle Tucker <kyleatucker@gmail.com>
2014-04-03 10:45:36 -0400

    Bump version for release v0.3.0

--- sugar.php
+++ sugar.php
@@ -6,7 +6,7 @@
  * Author URI:  https://github.com/asmbs
  * License:     MIT License
  * License URI: http://opensource.org/licenses/MIT
- * Version:     0.1.0
+ * Version:     0.3.0
  */
 
 // Don't forget, plugin_dir_path adds a trailing slash.
